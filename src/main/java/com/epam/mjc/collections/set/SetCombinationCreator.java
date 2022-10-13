143e744c1b439244007aa311801c996a367e7755
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-13 08:36:20 +0500

    Update SetCombinationCreator.java

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -4,6 +4,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetCombinationCreator {
-    public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+    public  Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+
+
+        thirdSet.removeIf(firstSet::contains);
+        thirdSet.removeIf(secondSet::contains);
+
+        firstSet.removeIf(obj -> !secondSet.contains(obj));
+        firstSet.removeIf(thirdSet::contains);
+
+        HashSet<String> res = new HashSet<>(firstSet);
+
+        res.addAll(thirdSet);
+
+
+        return res;
     }
 }
