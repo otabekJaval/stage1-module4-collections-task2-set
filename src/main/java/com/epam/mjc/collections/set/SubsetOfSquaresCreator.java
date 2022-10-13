24090e44c9cc6766deae1ed918aa2e48e227f4d6
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-13 08:00:20 +0500

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,22 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
+
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+
+        TreeSet<Integer>res=new TreeSet<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            int abs = Math.abs(sourceList.get(i));
+            abs = abs * abs;
+            res.add(abs);
+        }
+
+        return res.subSet(lowerBound,true, upperBound,true);
+
     }
 }
